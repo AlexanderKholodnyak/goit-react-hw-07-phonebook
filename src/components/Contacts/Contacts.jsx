a49251d2c6f94695abbf9c895ab1d97d5a071d36
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2021-02-07 00:09:18 +0200

    fin

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import * as operations from '../../redux/operations';
 import { getVisibleContacts } from '../../redux/selectors';
 import s from './Contacts.module.css';
@@ -7,6 +8,10 @@ function Contacts() {
   const contacts = useSelector(getVisibleContacts);
   const dispatch = useDispatch();
   const onDeleteContact = id => dispatch(operations.deleteContact(id));
+
+  useEffect(() => {
+    dispatch(operations.fetchContacts());
+  }, [dispatch]);
 
   return (
     <ul>
@@ -30,26 +35,3 @@ function Contacts() {
 }
 
 export default Contacts;
-
-// const Contacts = ({ contacts, onDeleteContact }) => (
-//   <ul>
-//     {contacts.map(({ id, name, number }) => (
-//       <li key={id} className={s.container}>
-//         <p className={s.name}>
-//           {name}: {number}{' '}
-//         </p>
-//         {
-//           <button
-//             className={s.button}
-//             type="button"
-//             onClick={() => onDeleteContact(id)}
-//           >
-//             Удалить
-//           </button>
-//         }
-//       </li>
-//     ))}
-//   </ul>
-// );
-
-// export default Contacts;
